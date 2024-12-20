bc46366d08b5f24c20c4ce7a0d57e9f6a550ebe9
mentalblood0 <necheporenko.stepan@yandex.ru>
2024-12-21 02:53:40 +0300

    'txt' extension even for channels with '.' in id

--- cache.nim
+++ cache.nim
@@ -5,8 +5,7 @@ type Cache* = object
   path: string
 
 proc new_cache*(name: string): Cache =
-  result.path =
-    string (get_data_dir() / "podcaster".Path / name.Path).add_file_ext "txt"
+  result.path = (get_data_dir() / "podcaster".Path / name.Path).string & ".txt"
   log(lvl_debug, &"new cache at {result.path}")
   if file_exists result.path.Path:
     for l in lines result.path:
