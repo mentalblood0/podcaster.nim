b46e5ff18bdaf9a405ecb19b54bd5e9694bc6b83
mentalblood0 <necheporenko.stepan@yandex.ru>
2024-12-22 22:23:28 +0300

    simplify config loading

--- podcaster.nim
+++ podcaster.nim
@@ -3,7 +3,7 @@
 # by the University of Cambridge, England. Source can be found at
 # ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/
 
-import std/[options, logging, json, appdirs, paths, cmdline, strutils, strformat, nre]
+import std/[options, logging, json, paths, cmdline, strutils, strformat, nre, os]
 
 import downloader
 import uploader
@@ -63,9 +63,8 @@ proc process_task[T](podcaster: Podcaster, task: ClassifiedTask[T]) =
     collector.on_uploaded(item, some(downloaded))
 
 when is_main_module:
-  let config = (
-    parse_json read_file string get_config_dir() / "podcaster".Path / Path param_str 1
-  ).to Config
+  let config =
+    (parse_json read_file get_config_dir().string / "podcaster" / param_str 1).to Config
 
   ytdlp_proxy = config.ytdlp_proxy
   temp_files_dir = config.temp_files_dir
