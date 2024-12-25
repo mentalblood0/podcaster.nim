69c60c9de907a84fed6755d18bd0e25d248cb487
mentalblood0 <necheporenko.stepan@yandex.ru>
2024-12-26 00:45:14 +0300

    take arbitrary number of configs _names_

--- podcaster.nim
+++ podcaster.nim
@@ -82,29 +82,31 @@ proc process_task[T](podcaster: Podcaster, task: ClassifiedTask[T]) =
     collector.on_uploaded(item, some(downloaded))
 
 when is_main_module:
-  let config =
-    (parse_json read_file get_config_dir() / "podcaster" / param_str 1).to Config
+  for config_name in command_line_params():
+    let config = (
+      parse_json read_file get_config_dir() / "podcaster" / config_name & ".json"
+    ).to Config
 
-  ytdlp_proxy = config.ytdlp_proxy
-  temp_files_dir = config.temp_files_dir
+    ytdlp_proxy = config.ytdlp_proxy
+    temp_files_dir = config.temp_files_dir
 
-  add_handler new_console_logger(fmt_str = "$date $time $levelname ")
-  case config.log_level
-  of "debug":
-    set_log_filter lvl_debug
-  of "info":
-    set_log_filter lvl_info
-  of "error":
-    set_log_filter lvl_error
+    add_handler new_console_logger(fmt_str = "$date $time $levelname ")
+    case config.log_level
+    of "debug":
+      set_log_filter lvl_debug
+    of "info":
+      set_log_filter lvl_info
+    of "error":
+      set_log_filter lvl_error
 
-  for t in config.tasks:
-    try:
-      if is_some t.url.match t.url.BandcampUrl.url_regex:
-        config.podcaster.process_task ClassifiedTask[BandcampUrl](source: t)
-      elif is_some t.url.match t.url.YoutubeUrl.url_regex:
-        config.podcaster.process_task ClassifiedTask[YoutubeUrl](source: t)
-      else:
-        raise new_exception(UnsupportedUrlError, &"No module support url '{t.url}'")
-    except:
-      remove_temp_files()
-      raise
+    for t in config.tasks:
+      try:
+        if is_some t.url.match t.url.BandcampUrl.url_regex:
+          config.podcaster.process_task ClassifiedTask[BandcampUrl](source: t)
+        elif is_some t.url.match t.url.YoutubeUrl.url_regex:
+          config.podcaster.process_task ClassifiedTask[YoutubeUrl](source: t)
+        else:
+          raise new_exception(UnsupportedUrlError, &"No module support url '{t.url}'")
+      except:
+        remove_temp_files()
+        raise
